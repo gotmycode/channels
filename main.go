d03b6a30dc945201b64f7568340aabe6acc401d4
Yvan Go <Galaxy@Yvans-MacBook-Air.fritz.box>
2018-05-30 16:50:18 +0200

    updated main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 //status checker - whether or not certain sites respond to traffic
@@ -41,8 +42,38 @@ func main() {
 	//fmt.Println(<-c)
 
 	//c inspired for loop
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	//for i := 0; i < len(links); i++ {
+	//fmt.Println(<-c)
+
+	// infinite loop
+	//for {
+
+	//use range with channel
+	//range c - wait for channel to return some value
+	//after channel has returned some value, assign to variable "l" (link), then run the body of the for loop
+	for l := range c {
+
+		//function literal (lambda in ruby,python, c#) (anonymous func in php, javascript)
+		//function literal is an unnamed function to wrap some code that can be executed later
+		//go func() {
+
+		//pass on link to func (as opposed to above implementation)
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			//warning expected here as variable l is declared outside of the func literal scope
+			//trying to reference a varibale that is being maintained (or used) by another go routine
+			//pass on link instead of just l
+			checkLink(link, c)
+
+		}(l) // add () to invoke function literal, pass on the value "l" as argument
+
+		//time.Sleep(5 * time.Second) - not a good implementation, messages throttle
+
+		//go checkLink(l, c)
+
+		//receiving value through a channel is a blocking operation
+		//checklink expects two arguments(see func checklink), first arg link as string, 2nd arg chan as string
+		//go checkLink(<-c, c)
 	}
 }
 
@@ -57,12 +88,18 @@ func checkLink(link string, c chan string) {
 		fmt.Println(link, "might be down!")
 
 		//send a message into the channel
-		c <- "Might be down I think"
+		//c <- "Might be down I think"
+
+		//pass the link to channel
+		c <- link
 		return
 	}
 	fmt.Println(link, "is up!")
 
 	//send a message into the channel
-	c <- "Yes, it is up"
+	//c <- "Yes, it is up"
+
+	//pass link to channel
+	c <- link
 
 }
